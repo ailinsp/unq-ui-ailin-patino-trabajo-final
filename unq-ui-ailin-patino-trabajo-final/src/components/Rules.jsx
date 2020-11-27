900a2da468f6097d82d5b3bd7dcdd341e5415a89
Ailin Patiño <ailinsp50@gmail.com>
2020-11-27 00:03:41 -0300

    agrego reglas

--- unq-ui-ailin-patino-trabajo-final/src/components/Rules.jsx
+++ unq-ui-ailin-patino-trabajo-final/src/components/Rules.jsx
@@ -1,11 +1,50 @@
 import React from 'react';
+import rules from './../pictures/RPSLSexplained.jpg'
+import { List, Typography } from 'antd';
+
 
 
 function Rules(){
+    const data = [
+        'Tijera corta a Papel',
+        'Papel tapa a Piedra',
+        'Piedra aplasta a Lagarto',
+        'Lagarto envenena a Spock',
+        'Spock rompe a Tijera',
+        'Tijera decapita a Lagarto',
+        'Lagarto devora a Papel',
+        'Papel desautoriza a Spock',
+        'Spock vaporiza a Piedra',
+        'y como siempre, Piedra aplasta a Tijera'
+      ];
+      
     
     return(
-        <div> 
-           <h>ACA VAN LAS REGLAS</h>
+        <div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                }}> 
+                <img alt={""} src={rules} style={{width:'500px',height:'500px'}}/>
+            </div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                }}>
+                <List
+                    bordered
+                    dataSource={data}
+                    renderItem={item => (
+                        <List.Item>
+                            <Typography.Text></Typography.Text> {item}
+                        </List.Item>
+                        )}
+                />
+                
+
+            </div>
         </div>
     )
 }
