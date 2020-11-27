11d65b8e1df50b7f6326fed02e702b447a15e155
Ailin Patiño <ailinsp50@gmail.com>
2020-11-26 23:34:19 -0300

    acomodo un poco landing

--- unq-ui-ailin-patino-trabajo-final/src/components/Landing.jsx
+++ unq-ui-ailin-patino-trabajo-final/src/components/Landing.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { Row, Col } from 'antd';
+import {NavLink} from 'react-router-dom';
+import rules from './../pictures/RULES.jpg'
+import play from './../pictures/PLAY.jpg'
+
 
 
 function Landing(){
     
     return(
-        <div> 
-           <h>ACA VA LA LANDING</h>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '80vh',
+            alignItems: 'center',
+            justifyContent: 'center'
+            }}> 
+
+            <Row gutter={[16, 24]}>
+                <Col className="gutter-row" span={6} >
+                    <NavLink to={`/rules`}>
+                        <img alt={""} src={rules} style={{width:'200px',height:'77px'}}/>
+                    </NavLink>
+                </Col>
+                <Col className="gutter-row" span={6} >
+
+                    <NavLink to={`/play`}>
+                        <img alt={""} src={play} style={{width:'200px',height:'77px'}}/>
+                    </NavLink>
+                </Col>
+            </Row>
+
         </div>
     )
 }
