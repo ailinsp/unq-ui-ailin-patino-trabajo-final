b704e6de4e386638cc6444c644eb46c19bb53ada
Ailin Patiño <ailinsp50@gmail.com>
2020-12-03 21:52:21 -0300

    update del onChange de las opciones

--- unq-ui-ailin-patino-trabajo-final/src/components/Options.jsx
+++ unq-ui-ailin-patino-trabajo-final/src/components/Options.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
-import lizard from './../pictures/LIZARD.jpg'
-import paper from './../pictures/PAPER.jpg'
-import rock from './../pictures/ROCK.jpg'
-import scissors from './../pictures/SCISSORS.jpg'
-import spock from './../pictures/SPOCK.jpg'
+import lizard from './../pictures/lizard.jpg'
+import paper from './../pictures/paper.jpg'
+import rock from './../pictures/rock.jpg'
+import scissors from './../pictures/scissors.jpg'
+import spock from './../pictures/spock.jpg'
 
 const Options = ({setAnOption}) => {
 
     return (
         <div>
-            <div onClick={() => {}}>
+            <div onClick={() => setAnOption('rock')}>
                 <img src={rock} alt='rock' style={{width:'110px',height:'110px'}}/>
             </div>
             
-            <div onClick={() => {}}>
+            <div onClick={() => setAnOption('paper')}>
                 <img src={paper} alt='paper' style={{width:'110px',height:'110px'}}/>
             </div>
             
-            <div onClick={() => {}}>
+            <div onClick={() => setAnOption('scissors')}>
                 <img src={scissors} alt='scissors'style={{width:'110px',height:'110px'}}/> 
             </div>
 
-            <div onClick={() => {}}>
+            <div onClick={() => setAnOption('lizard')}>
                 <img src={lizard} alt='lizard'style={{width:'110px',height:'110px'}} /> 
             </div>
 
-            <div onClick={() => {}}>
+            <div onClick={() => setAnOption('spock')}>
                 <img src={spock} alt='spock' style={{width:'110px',height:'110px'}}/> 
             </div>
         </div>
